3c63ffaa9d46addb4487964b7bda830ae26c8b6e
Gaurav0002 <gauravking918@gmail.com>
2024-03-17 16:20:09 +0530

    Final Patch

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -21,10 +21,11 @@ const Cart = () => {
         </div>
         <div className='item3'>
           <div className='button_parent'>
-            <p>Clear the Cart:</p>
-            <div className='cart_clear_button' >
-              <button className='mybutton' onClick={() => handleclearcart()} >Clear</button>
+            <p className='pb-2' >Clear the Cart:</p>
+            <div className='cart_clear_button   ' >
+              <button className='mybutton   ' onClick={() => handleclearcart()} >Clear</button>
             </div>
+            {/* bg-red-600 text-white p-5 py-1 px-3 absolute top-2 left-2 z-10 rounded-md text-lg */}
           </div>
         </div>
         <div className='item4'></div>
@@ -45,7 +46,7 @@ const Cart = () => {
                   <p className='price_tag' >Price: {data?.card?.info?.price / 100}</p>
                   {/* <p className='' > Rating: {data?.card?.info?.ratings?.aggregatedRating?.rating} </p> */}
 
-                  <img className='menu_image'
+                  <img className='menu_image '
                     src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/${data?.card?.info?.imageId}`}
                     alt='Menu pic'
                   />
